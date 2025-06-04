0f91f07a34aeff493ca81465e421e47e3de25237
an-pham08 <Phamtrangan2008@gmail.com>
2025-06-04 19:13:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 // Thay đổi nội dung búc thư ở đây
-var letterContent =" Cảm ơn em đã dành thời gian để nhìn những trò trẻ con của anhh và anh có những điều muốn gởi gắm đến tình iu của anhh❤️. Cảm ơn em đã đến bên anhh những lúc anh cô đơn buồn tủi nhất.Cuộc sống thực sự chẳng có ý nghĩa gì nếu không có em. Em làm cho anh luôn cảm thấy đặc biệt và hoàn hảo. Anh chẳng biết nói gì ngoài lời cảm ơn em, anh rất yêu em💕"
+var letterContent =" Gửi em,
+
+Vậy là mình đã bên nhau 200 ngày rồi. Anh biết con số này không nói lên tất cả, nhưng với anh, đó là quãng thời gian đủ để hiểu rằng: em rất quan trọng trong cuộc sống của anh.
+
+Cảm ơn em vì đã luôn ở bên, vì những lúc kiên nhẫn lắng nghe, vì đã chấp nhận cả những phần chưa hoàn hảo của anh. Anh biết mình đôi khi còn vụng về, chưa nói được hết những điều em xứng đáng được nghe – nhưng thật lòng, anh trân trọng em và những gì cả hai đang có.
+
+Mong rằng những ngày tiếp theo, mình vẫn sẽ cùng nhau cố gắng, cùng trưởng thành và cùng giữ gìn tình cảm này thật lâu.
+
+Thương em.
+An
+Anh yêu của bé
+
+"
 
 // Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
 durationWrite = 50 
